c6f25b886f8175c7478f48dc2eddbdb5c5e5e1dd
Mrunali Tanaji Bagal <bagalmrunali30@gmail.com>
2026-07-13 22:23:31 +0530

    Add files via upload

---
+++ selection_sort.py
@@ -0,0 +1,33 @@
+# def selectionSort(li):
+
+#     size =len(li)
+#     for i in range(0,size-1):
+#         min_ind=i
+#         for j in range(i+1,size):
+#          if(li[min_ind] > li[j]):
+#           min_ind=j
+
+#         li[i],li[min_ind]=li[min_ind],li[i] 
+#         print(li)  
+
+
+# li=[ 60,50,89,90,20,30]
+# selectionSort(li)
+# print('After sorting:',li)
+
+
+# nested list
+# ladder bord
+
+# li=[10,20,[30 ,40,50,[60,[70,80]]]]
+# lenth of list
+
+# print(li[2])
+# print(li[2][0])
+# print(li[2][3][0])
+# print(li[2][3][1][1])
+# print(li[2][1])
+# print(li[2][3][0])
+
+# inbuild method of list
+
