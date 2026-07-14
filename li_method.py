13fc712bfddd2d5d8274e86f9577811b2b99285f
Mrunali Tanaji Bagal <bagalmrunali30@gmail.com>
2026-07-14 21:54:05 +0530

    Add files via upload

---
+++ li_method.py
@@ -0,0 +1,32 @@
+li=[30,10,40,20]
+
+# # li.append((50,60))
+
+# # li.clear()
+
+# li2=li.copy()
+# li3=li
+# li.append(50)
+
+# print(li2)
+# print(li3)
+
+# print(li)
+
+# li.extend([60,70,70])
+# print(li.extend(20))
+
+
+
+
+# print(li.index(20))
+# li.insert(2,50)
+# print(li)
+# li.pop(2)
+# #  li.remove(10)
+# print(li)
+
+li.revers(40)
+print(li)
+
+
