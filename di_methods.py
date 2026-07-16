df9a83fd058982e2947c36bed678d9534a0126cd
Mrunali Tanaji Bagal <bagalmrunali30@gmail.com>
2026-07-16 15:45:54 +0530

    Add files via upload

---
+++ di_methods.py
@@ -0,0 +1,38 @@
+# di={'id':101,'name':'rohit','sal':75000}
+
+# di.clear()
+# di2 =di.copy()
+
+# print(di.get('eid','key not found'))
+
+# print(di.items())
+# print(di.keys())
+# di.pop('name')#return the deleted value 
+# di.popitem()
+# di.update({'age':23,'address':'pune'})
+
+
+# print(di)   
+# print(di.values())  
+
+# di['salary']=10000
+# print(di)
+
+##set
+# 1. {}with only values
+s1={23,34,14,56,67,'w',7.90,14}
+# s1 =set()
+# s1 ={}
+
+# 2. hetrogenuous
+
+# 3.Unorderd
+
+# 4.Mutable,not editable 
+
+s1.add(50)
+
+# 5.unique
+
+# print(type(s1))
+print(s1)
