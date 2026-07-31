3adfc191fa5b58cfb4b56db6cd57c126baf1c31f
Mrunali Tanaji Bagal <bagalmrunali30@gmail.com>
2026-07-31 23:53:49 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,19 @@
+# 1.structure:()
+tu=(30,40,50,10,10)
+# tu=(10,)
+# print(type(tu))
+
+# 2.types of  elements are stor : hetrogeous
+# tu=(30,40,50,10,'a',2.3)
+
+# 3.sequence:ordered
+
+# 4.Changable :Inmutable
+#  tu[00=50]
+
+
+# 5. Duplication:allowed
+
+print(type(tu))
+print(tu)
+      
