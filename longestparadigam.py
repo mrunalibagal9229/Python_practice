841686429f17d75fc738e8d31e2942e58e201eb6
Mrunali Tanaji Bagal <bagalmrunali30@gmail.com>
2026-08-31 22:33:11 +0530

    Add files via upload

---
+++ longestparadigam.py
@@ -0,0 +1,11 @@
+def is_palindrome(s: str) -> bool:
+    left, right = 0, len(s) - 1
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+
+print(is_palindrome("level"))   
+print(is_palindrome("python"))  
